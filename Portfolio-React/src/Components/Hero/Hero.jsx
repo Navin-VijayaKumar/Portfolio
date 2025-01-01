cfc987aa0a5e930c47d97d72c4714e759a7230a9
Navin-VijayaKumar <navinv10122004@gmail.com>
2025-01-01 13:21:55 +0530

    commit sixteen

--- Portfolio-React/src/Components/Hero/Hero.jsx
+++ Portfolio-React/src/Components/Hero/Hero.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import './Hero.css'
-import profilepic from '../Assets/profilepic.jpg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from 'react';
+import './Hero.css';
+import profilepic from '../Assets/profilepic.jpg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
-        <img  className='pic' src={profilepic} alt=''></img>
-            <h1><span>Hello, I'am Navin,</span>A full stack developer</h1>
-            <p>A passionate Web developer have experience
-in creating full stack websites
-using Html, Css,Mongo DB,Node js and JavaScript and solving
-coding problems in coding platforms
-with languages like Java, Python and C</p>
-<div className="hero-action">
-    <div className="hero-cunnect">
+    <div id="home" className="hero">
+      <img className="pic" src={profilepic} alt="Profile" />
+      <h1>
+        <span>Hello, I'am Navin,</span>A full stack developer
+      </h1>
+      <p>
+        A passionate Web developer with experience in creating full stack websites using HTML, CSS, MongoDB, Node.js, and JavaScript, as well as solving coding problems on platforms using languages like Java, Python, and C.
+      </p>
+      <div className="hero-action">
+      <div className="hero-cunnect">
         <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink>
     </div>
-    <div className="hero-resume">
-        My Resume
+        <div className="hero-resume">
+          <a
+            href="/navinres.docx"
+            download="navinres.docx"
+            className="resume-link"
+          >
+            My Resume
+          </a>
+        </div>
+      </div>
     </div>
-</div>
-    </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
