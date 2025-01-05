94c9f2fad3fbb6a9ac6389d3acad3f687db45c0c
Navin-VijayaKumar <navinv10122004@gmail.com>
2025-01-05 23:41:15 +0530

    commit eightteen

--- Portfolio-React/src/App.jsx
+++ Portfolio-React/src/App.jsx
@@ -6,12 +6,14 @@ import Services from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import SkillSet from './Components/SkillSet/SkillSet'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <About/>
+      <SkillSet></SkillSet>
       <Services/>
       <MyWork/>
       <Contact/>
---
+++ Portfolio-React/src/Components/SkillSet/SkillSet.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './SkillSet.css' 
+import react from '../Assets/react.png'
+import java from '../Assets/java.png'
+import nodejs from '../Assets/nodejs.png'
+import mondb from '../Assets/mondb.png'
+
+const SkillSet = () => {
+  return (
+    <div className='rot'>
+        <div>
+
+        <h1>Skill Set</h1>
+        </div>
+        <div className="rot-items">
+
+        <div className='rot-item1'>
+
+        <img className='rot-img' src={react}></img>
+        </div>
+        <div className='rot-item1'>
+
+        <img className='rot-img' src={java}></img>
+        </div>
+        <div className='rot-item2'>
+
+        <img className='rot-img' src={nodejs}></img>
+        </div>
+        <div className='rot-item2'>
+
+        <img className='rot-img1' src={mondb}></img>
+        </div>
+        </div>
+
+    </div>
+
+)
+}
+
+export default SkillSet
