b8dfe5831b6be154d8c10962cfe139d81287ccfc
Navin-VijayaKumar <navinv10122004@gmail.com>
2025-01-06 14:18:14 +0530

    commit nineteen

--- Portfolio-React/src/Components/SkillSet/SkillSet.jsx
+++ Portfolio-React/src/Components/SkillSet/SkillSet.jsx
@@ -4,6 +4,8 @@ import react from '../Assets/react.png'
 import java from '../Assets/java.png'
 import nodejs from '../Assets/nodejs.png'
 import mondb from '../Assets/mondb.png'
+import fb from '../Assets/fb.png'
+import js from '../Assets/js.png'
 
 const SkillSet = () => {
   return (
@@ -21,6 +23,14 @@ const SkillSet = () => {
         <div className='rot-item1'>
 
         <img className='rot-img' src={java}></img>
+        </div>
+        <div className='rot-item1'>
+
+        <img className='rot-img' src={fb}></img>
+        </div>
+        <div className='rot-item2'>
+
+        <img className='rot-img' src={js}></img>
         </div>
         <div className='rot-item2'>
 
